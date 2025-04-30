363757beb56154d5eb6611fad0ea8dc56d5721a2
harveypw0503 <harveypw0503@gmail.com>
2025-04-29 20:32:36 -0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,20 +1,15 @@
+from django.shortcuts import render
 from rest_framework import generics
 from .models import Reminder
 from .serializers import ReminderSerializer
-from django.shortcuts import render
 
-# Create + List Reminders
 class ReminderListCreateView(generics.ListCreateAPIView):
-    queryset = Reminder.objects.all().order_by('remind_at')
+    queryset = Reminder.objects.all()
     serializer_class = ReminderSerializer
 
-# Retrieve, Update (reschedule), Delete Reminders
 class ReminderDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Reminder.objects.all()
     serializer_class = ReminderSerializer
 
-def reminders_page(request):     # links to viewing reminders
+def reminders_page(request):
     return render(request, 'reminders.html')
-
-def homepage(request):       # links to homepage
-    return render(request, 'reminders.html')  # different homepage change html
