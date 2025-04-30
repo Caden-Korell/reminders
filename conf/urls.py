e77619c19ca0ef0ebd91ce4d4aaa83620ae6d3e5
harveypw0503 <harveypw0503@gmail.com>
2025-04-29 20:32:11 -0500

    Update urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from .views import ReminderListCreateView, ReminderDetailView, reminders_page
 
 urlpatterns = [
-    path('', include('api.urls')),
+    path('reminders/', ReminderListCreateView.as_view(), name='reminder-list-create'),
+    path('reminders/<int:pk>/', ReminderDetailView.as_view(), name='reminder-detail'),
+    path('view-reminders/', reminders_page, name='view-reminders'),
 ]
